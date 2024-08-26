428aed76846018110c1c5fcfbd88bfded0a98f33
cpmoore0317 <cpmoore317@gmail.com>
2024-08-26 15:35:29 -0500

    Rendering app through src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
+// Get the root element from the DOM
+const rootElement = document.getElementById("root");
+
+// Create a root for React
+const root = ReactDOM.createRoot(rootElement);
+
 // Render the App component to the root element
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
