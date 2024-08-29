83b3a03b382a06d4690692a30137b9e4bda149b3
cpmoore0317 <cpmoore317@gmail.com>
2024-08-29 13:36:50 -0500

    Updating social links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,10 +5,10 @@ import githubImage from "../assets/github.svg";
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/parkermoore317/" target="_blank" rel="noopener noreferrer">
         <img src={linkedinImage} alt="LinkedIn" />
       </a>
-      <a href="https://github.com/your-repository" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/cpmoore0317/atlas-react-and-jsx" target="_blank" rel="noopener noreferrer">
         <img src={githubImage} alt="GitHub" />
       </a>
     </div>
